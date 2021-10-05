8d036734e35620f04348aced54f93a6dc5ced5f2
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2021-10-05 12:09:24 +0200

    dossier backend app + server

---
+++ app.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('./models/user');
+const Sauce = require('./models/sauce');
+
+
+
+/*--------------------------------------------------------*/
+
+mongoose.connect('mongodb+srv://kage59:shogun99@cluster0.dnnex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+                 
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+/*--------------------------------------------------------*/
+
+const app = express ();
+
+/*--------------------------------------------------------*/
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json());
+
+
+
+
+
+
+/*--------------------------------------------------------*/
+
+/*app.post('/api/auth/signup', (req, res, next) => {
+  delete req.body._id;
+    const user = new User({
+      ...req.body
+    });
+    user.save()
+      .then(() => res.status(201).json({ message: 'Utilisateur enregistrée !'}))
+      .catch(error => res.status(400).json({ error }));
+  
+
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Compte créé !'
+  });
+});
+
+app.use('/api/auth/signup', (req, res, next) => {
+  User.find()
+  .then(Signup => res.status(200).json(Signup))
+  .catch(error => res.status(400).json({ error }));
+  const signup = [
+  ];
+  res.status(200).json(signup);
+});*/
+
+/*--------------------------------------------------------*/
+
+/*app.post('/api/auth/login', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Compte connecté !'
+  });
+});
+
+
+app.use('/api/auth/login', (req, res, next) => {
+  const login = [
+  ];
+  res.status(200).json(login);
+});*/
+
+/*--------------------------------------------------------*/
+
+/*app.post('/api/sauces', (req, res, next) => {
+  delete req.body._id;
+    const sauce = new Sauce({
+      ...req.body
+    });
+    sauce.save()
+      .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
+      .catch(error => res.status(400).json({ error }));
+
+
+  console.log(req.body);
+  res.status(201).json({
+    message: 'sauce enregistrée !'
+  });
+});
+
+
+app.use('/api/sauces', (req, res, next) => {
+    Sauce.find()
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(400).json({ error }));
+
+
+  res.status(200).json(sauces);
+});*/
+
+/*--------------------------------------------------------*/
+
+module.exports = app;
+
+
+
+
+
+
+
+/*app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+  });
+
+app.use((req, res, next) => {
+res.status(201);
+next();
+});
+
+app.use ((req, res, next) => {
+res.json ({message : 'votre requête a bien été reçue!'})
+});
+
+app.use((req, res) => {
+    console.log('Réponse envoyée avec succès !');
+  });*/
+
