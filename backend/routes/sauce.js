9312ff08412ee2cfae05c4c285c8df340fd79b97
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2021-10-11 16:43:13 +0200

    ajout multer route createSauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
+const multer = require('../middleware/multer-config');
 
 // Routes
-router.post('/', sauceCtrl.createSauce);
+router.post('/', multer, sauceCtrl.createSauce);
 router.put('/:id', sauceCtrl.modifySauce);
 router.delete('/:id', sauceCtrl.deleteSauce);
 router.get('/:id', sauceCtrl.getOneSauce); 
