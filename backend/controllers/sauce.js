1eba178f645a2e31281e09411e9c0b7bca25118e
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2021-10-12 11:59:27 +0200

    ajout commentaires

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,14 +1,18 @@
 const Sauce = require('../models/Sauce');
 
 
-//créer sauce
+//créer nouvelle sauce
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
+
+    // Création d'un nouvel objet Sauce
     const sauce = new Sauce({
       ...sauceObject,
+      // Création de l'URL de l'image : http://localhost:3000/image/nomdufichier 
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
+    // Enregistrement de l'objet sauce dans la base de données
     sauce.save()
       .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
       .catch(error => res.status(400).json({ error }));
@@ -18,10 +22,12 @@ exports.createSauce = (req, res, next) => {
 //modifier sauce selectionnée
   exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
+     // Si il existe déjà une image
     {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body }; 
+    } : { ...req.body };
+      // Si il n'existe pas d'image
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
       .catch(error => res.status(400).json({ error }));
