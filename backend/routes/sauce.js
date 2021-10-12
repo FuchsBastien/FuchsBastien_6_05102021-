0b7d4546672e490da49ba112dbcb20a3d37dd0c9
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2021-10-12 11:09:19 +0200

    ajout auth route sauces

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
+const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 // Routes
-router.post('/', multer, sauceCtrl.createSauce);
-router.put('/:id', multer, sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
-router.get('/:id', sauceCtrl.getOneSauce); 
-router.get('/', sauceCtrl.getAllSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce); 
+router.get('/', auth, sauceCtrl.getAllSauce);
 
 
 module.exports = router;
